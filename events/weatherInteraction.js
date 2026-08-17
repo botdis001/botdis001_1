c407c141aa8576a986b83221eeedd605e0a31a8b
botdis001 <botdiscord001_1@hotmail.com>
2026-08-17 16:59:49 +0700

    Update weatherInteraction.js

--- events/weatherInteraction.js
+++ events/weatherInteraction.js
@@ -9,8 +9,8 @@ const {
     TextInputStyle
 } = require('discord.js');
 
-// URL เว็บของคุณบน Railway
-const WEB_URL ='https://botdis0011-production-c580.up.railway.app';
+// URL เว็บของคุณบน Railway (แก้ไขให้ถูกต้องไม่มีเว้นวรรค)
+const WEB_URL = 'https://botdis0011-production-c580.up.railway.app';
 
 const GEO_URL =
     'https://raw.githubusercontent.com/kongvut/thai-province-data/refs/heads/master/api/latest/province_with_district_and_sub_district.json';
